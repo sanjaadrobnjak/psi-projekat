58ab6378c5fd296f79c0abfc50f5e6e714b87f7a
Ivan Cancar <ci210604d@student.etf.bg.ac.rs>
2024-05-26 01:04:04 +0200

    Ispravka baga: admin pristupa sajtu za igru
    Change-Id: I1d0c5e27bf9ccd1b11afc7d53849c7983c1178c0

--- Implementacija/app/views.py
+++ Implementacija/app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.contrib.auth import logout
 
 def index(request):
     if request.user.is_authenticated:
+        if request.user.is_staff:
+            logout(request)
+            return render(request, 'app/index.html')
         return redirect('home-view')
     return render(request, 'app/index.html')
